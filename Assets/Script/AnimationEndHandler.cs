45b4e5d7910c2301c2b08e60eaabb75c814b0c8e
wkb63744 <woochaemin123@naver.com>
2025-08-07 00:01:13 +0900

    .

--- Assets/Script/AnimationEndHandler.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class AnimationEndHandler : StateMachineBehaviour
-{
-    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    {
-        animator.gameObject.GetComponent<Player>().OnAttackEnd();
-    }
-}
