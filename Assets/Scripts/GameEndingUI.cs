35236a00f980041630b96c58c507365811dcd1f5
Kim Gyeonghun <gyhub.dev@gmail.com>
2025-08-26 03:25:56 +0900

    feat: build setting

--- Assets/Scripts/GameEndingUI.cs
+++ Assets/Scripts/GameEndingUI.cs
@@ -2,8 +2,6 @@ using System;
 using System.Collections;
 using UnityEngine;
 using TMPro;
-using Microsoft.Unity.VisualStudio.Editor;
-using Unity.VisualScripting;
 
 public class GameEndingUI : MonoBehaviour
 {
